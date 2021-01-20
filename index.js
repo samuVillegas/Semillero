1c7a999349d5f565396b6bfb24f6bba934083462
samuVillegas <villegassamuel25@gmail.com>
2021-01-20 17:22:30 -0500

    Se añade reto

--- index.js
+++ index.js
@@ -12,13 +12,10 @@ app.use(express.json());
 //Routes
 app.use('/api',routes);
 
-app.listen(8086,()=>{
-    console.log('Corriendo en el puerto 8086');
-});
 
 //Variables de entorno
-/*app.set('port',process.env.PORT || 5000); // Si existe PORT sino 5000 Asignar variable
+app.set('port',process.env.PORT || 5000); // Si existe PORT sino 5000 Asignar variable
 
 app.listen(app.get('port'),() =>{
     console.log(`Aplicacion corriendo en el puerto ${app.get('port')}`);
-});*/
+});
